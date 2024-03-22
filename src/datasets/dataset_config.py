c42c97d06b14c9421fb61d92af0530c551f4a2c8
eliasladis <152519990+eliasladis@users.noreply.github.com>
2024-03-22 23:48:19 +0200

    Update dataset_config.py
    DEBUGGING : Changed class order's number of classes from 0-19 to 0-1

--- src/datasets/dataset_config.py
+++ src/datasets/dataset_config.py
@@ -93,7 +93,7 @@ dataset_config = {
     'ucmerced' : {
         'path': '/content/drive/MyDrive/Datasets/UCMerced_LandUse/Images',
         'class_order' : [
-            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+            0, 1, 2]
     }
 }
 
